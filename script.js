63650004568db581b2da9e552092262dc241f0cf
Artem <artem.ivashchenko.exe@gmail.com>
2023-04-14 16:03:10 +0300

    re-made for lab

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const button = document.getElementsByClassName('hamburger')[0];
-const nav = document.getElementsByClassName('header__nav')[0];
+// const button = document.getElementsByClassName('hamburger')[0];
+// const nav = document.getElementsByClassName('header__nav')[0];
 
-button.addEventListener('click', () => {
-  nav.classList.toggle('active');
-})
+// button.addEventListener('click', () => {
+//   nav.classList.toggle('active');
+// })
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('body--with-menu');
+  } else {
+    document.body.classList.remove('body--with-menu');
+  }
+});
