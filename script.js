3ec77925a5c4b02b8bc5cabdd2c0b12eb733e3d7
Maksim Pozhidaev <maxoovka1@gmail.com>
2024-08-08 13:20:09 +0300

    chore: run formatter

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const button = document.querySelector('i.hamburger');
-const nav = document.querySelector('div.header__nav');
+const button = document.querySelector("i.hamburger");
+const nav = document.querySelector("div.header__nav");
 
 console.log(nav.clientHeight);
 
-button.addEventListener('click', () => {
-  nav.classList.toggle('active');
-})
+button.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
